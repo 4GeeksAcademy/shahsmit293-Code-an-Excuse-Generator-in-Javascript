4687ef7dafca6cf19f3dcaf426b70c1eb18fc2c4
shahsmit293 <138520584+shahsmit293@users.noreply.github.com>
2023-08-04 23:29:17 +0000

    Final changes

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  var person = ["father", "mother", "grandma", "grandfather"];
+  var action = ["kicked", "snapped", "minced", "throw"];
+  var work = ["homework", "project", "report", "presentation"];
+  var time = ["on monday", "on thursday", "on friday", "on sunday"];
+
+  var who = person[Math.floor(Math.random() * person.length)];
+  var did = action[Math.floor(Math.random() * action.length)];
+  var what = work[Math.floor(Math.random() * work.length)];
+  var when = time[Math.floor(Math.random() * time.length)];
+
+  var sentence = `My ${who} ${did} my ${what} ${when}`;
+  var text = document.querySelector("#excuseme");
+  text.textContent = sentence;
 };
